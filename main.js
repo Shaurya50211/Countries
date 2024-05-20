20e35da2a195ba5427d68331e2fdc863d3716ee6
Shaurya Gupta <shaurya50211@gmail.com>
2024-05-20 16:19:08 -0400

    finished home page and working on details page

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const countries = []
 
-fetch("./data.json")
-.then(response => {
-  return response.json();
-})
-.then(data => {
-    addCountry(data);
-    countries.push(...data);
-})
-.catch(err => {
-    console.error(err);
-})
-
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
+fetch("./data.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        addCountry(data);
+        countries.push(...data);
+    })
+    .catch(err => {
+        console.error(err);
+    })
 
 function addCountry(countries) {
     countries.forEach(country => {
@@ -55,6 +55,21 @@ function addCountry(countries) {
     </div>
         `
     });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            const countryName = card.querySelector(".name").innerHTML;
+            const country = countries.find(country => country.name === countryName);
+
+            redirctToDetailsPage(country);
+        });
+    });
+}
+
+function redirctToDetailsPage(country) {
+    
 }
 
 
@@ -115,3 +130,69 @@ function findMatches(wordToMatch, countries) {
         return country.name.match(regex);
     });
 }
+
+
+const darkMode = document.querySelector(".dark-mode");
+
+darkMode.addEventListener("click", () => {
+    if (document.querySelector(".dark-mode p").innerHTML === "Light Mode") {
+        document.querySelector(".dark-mode p").innerHTML = "Dark Mode";
+        document.documentElement.style.setProperty('--dark-bg', '#fafafa');
+        document.documentElement.style.setProperty('--light-dark', '#ffffff');
+        document.documentElement.style.setProperty('--white-color', '#121417');
+    } else {
+        document.querySelector(".dark-mode p").innerHTML = "Light Mode";
+        document.documentElement.style.setProperty('--dark-bg', '#212c36');
+        document.documentElement.style.setProperty('--light-dark', '#2c3743');
+        document.documentElement.style.setProperty('--white-color', '#f9f8fa');
+    }
+})
+
+
+// Filter by region
+
+const regionSelect = document.getElementById("region");
+
+regionSelect.addEventListener("change", filterByRegion);
+
+function filterByRegion() {
+    const selectedRegion = regionSelect.value;
+    const cardsDiv = document.querySelector(".cards");
+    cardsDiv.innerHTML = "";
+
+    if (selectedRegion === "All") {
+        addCountry(countries);
+        return;
+    } else {
+        countries.map(country => {
+            if (country.region === selectedRegion) {
+                cardsDiv.innerHTML += `
+                <div class="card">
+                <img src="${country.flag}"
+                    alt="flag">
+                <p class="name">${country.name}</p>
+            
+                <div class="stats">
+                    <span>
+                        <strong>Population:</strong>
+                        <span>${numberWithCommas(country.population)}</span>
+                    </span>
+            
+                    <span>
+                        <strong>Region:</strong>
+                        <span>${country.region}</span>
+                    </span>
+            
+                    <span>
+                        <strong>Capital:</strong>
+                        <span>${country.capital}</span>
+                    </span>
+                </div>
+            </div>
+                `
+            }
+        });
+    }
+}
+
+// Card clicked, display details of country in a new page
